60757d6c839c11edf135e588275676128de2a7e0
littlesamakfox <securityofanime@gmail.com>
2022-04-04 22:17:37 +0900

    private instance 테스트 2개 추가

--- instance.tf
+++ instance.tf
@@ -16,3 +16,16 @@ resource "aws_eip" "k5s_bastion_eip" {
     instance = aws_instance.k5s_bastion.id
     depends_on = [aws_internet_gateway.k5s_igw]
 }
+
+resource "aws_instance" "k5s_instance" {
+    ami = "ami-033a6a056910d1137"
+    availability_zone = aws_subnet.k5s_private_subnet_1.availability_zone
+    instance_type = "t2.micro"
+    key_name = aws_key_pair.k5s_key.key_name
+    count=2
+    vpc_security_group_ids = [
+        aws_default_security_group.k5s_vpc_sg_default.id,
+        aws_security_group.k5s_sg_bastion.id
+    ]
+    subnet_id = aws_subnet.k5s_private_subnet_1.id
+}
